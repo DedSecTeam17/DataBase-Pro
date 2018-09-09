317febde3a934db57e562751e14f9b9fbb764be6
Sherlocked1 <40625231+Sherlocked1@users.noreply.github.com>
2018-09-09 09:35:42 +0200

    TransactionsSingleton and builder

--- src/sample/MarketModel/Transaction.java
+++ src/sample/MarketModel/Transaction.java
@@ -3,50 +3,85 @@ package sample.MarketModel;
 public class Transaction {
 
 
+    private int id;
     private String productName;
+    private int sellingPrice;
+    private int quantity;
     private int profit;
     private String created_at;
 
     public Transaction(TransactionBuilder transactionBuilder) {
+        setId(transactionBuilder.id);
         setProductName(transactionBuilder.productName);
         setProfit(transactionBuilder.profit);
         setCreated_at(transactionBuilder.created_at);
+        setSellingPrice(transactionBuilder.sellingPrice);
+        setQuantity(transactionBuilder.quantity);
     }
 
     public static TransactionBuilder newTransaction() {
         return new TransactionBuilder();
     }
 
-    public String getProductName() {
-        return productName;
+    //#region Getters and setters
+
+    public int getId() {
+        return id;
     }
 
-    public void setProductName(String productName) {
-        this.productName = productName;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(int sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getProductName() {
+        return productName;
     }
 
     public int getProfit() {
         return profit;
     }
 
+    public String getCreated_at() {
+        return created_at;
+    }
+
     public void setProfit(int profit) {
         this.profit = profit;
     }
 
-    public String getCreated_at() {
-        return created_at;
+    public void setProductName(String productName) {
+        this.productName = productName;
     }
 
     public void setCreated_at(String created_at) {
         this.created_at = created_at;
     }
 
+    //#endregion
 
     public static class TransactionBuilder {
-
+        private int id;
         private String productName;
         private int profit;
         private String created_at;
+        private int sellingPrice;
+        private int quantity;
 
 
         public TransactionBuilder productName(String productName) {
@@ -63,6 +98,21 @@ public class Transaction {
 
         public TransactionBuilder created_at(String created_at) {
             this.created_at = created_at;
+            return this;
+        }
+
+        public TransactionBuilder quantity(int quantity) {
+            this.quantity = quantity;
+            return this;
+        }
+
+        public TransactionBuilder sellingPrioce(int sellingprice) {
+            this.sellingPrice = sellingprice;
+            return this;
+        }
+
+        public TransactionBuilder id(int id) {
+            this.id = id;
             return this;
         }
 
--- src/sample/MarketSingeltons/TransactionDataBaseSingelton.java
+++ src/sample/MarketSingeltons/TransactionDataBaseSingelton.java
@@ -1,8 +1,13 @@
 package sample.MarketSingeltons;
 
+import sample.Atuhentication.Auth;
+import sample.Debugging.Log;
+import sample.MarketModel.Category;
 import sample.MarketModel.Product;
 import sample.MarketModel.Transaction;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionDataBaseSingelton {
@@ -15,22 +20,96 @@ public class TransactionDataBaseSingelton {
     }
     public  String addTransaction(Transaction Transaction)
     {
-        return  "message come from server";
+        String data_base_message = "";
+        try {
+            Connection connection = Config.getInstance().getConnection();
+            String sql = "INSERT INTO MARKETTRANSACTIONS(T_USER_EMAIL,T_PRODUCT_NAME,TRANSACTION_DATE,SELLING_PRICE,QUANTITY,TRANSACTION_TOTAL,TRANSACTION_PROFIT) VALUES(?,?,?,?,?,?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, Auth.getInstance().getCurrentUser());
+            preparedStatement.setString(2, Transaction.getProductName());
+            preparedStatement.setDate(3, Date.valueOf(Transaction.getCreated_at()));
+            preparedStatement.setInt(4, Transaction.getSellingPrice());
+            preparedStatement.setInt(5,Transaction.getQuantity() );
+            preparedStatement.executeUpdate();
+
+
+        } catch (SQLException e) {
+            Log.i(e.getMessage());
+            data_base_message = e.getMessage();
+        } catch (ClassNotFoundException e) {
+            data_base_message = e.getMessage();
+        }
+
+        return data_base_message;
 
     }
     public  String deleteTransaction(Transaction Transaction)
     {
-        return  "message come from server";
-
+        String meaasge = "";
+        String sql = String.format("DELETE FROM MARKETTRANSACTIONS WHERE  Transaction_id='%d'", Transaction.getId());
+        try {
+            Connection connection = Config.getInstance().getConnection();
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        } catch (SQLException | ClassNotFoundException e) {
+            meaasge = e.getMessage();
+        }
+        return meaasge;
     }
     public  String updateTransaction(Transaction Transaction)
     {
-        return  "message come from server";
+        String data_base_message = "";
+        try {
+            Connection connection = Config.getInstance().getConnection();
+            String sql = "UPDATE MarketProduct SET  SELLING_PRICE = ? ,QUANTITY = ? WHERE  Transaction_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, Transaction.getSellingPrice());
+            preparedStatement.setInt(2, Transaction.getQuantity());
+            preparedStatement.setInt(3, Transaction.getId());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            Log.i(e.getMessage());
+            data_base_message = e.getMessage();
+        } catch (ClassNotFoundException e) {
+            data_base_message = e.getMessage();
+        }
+
+        return data_base_message;
 
     }
 
-    public List<Transaction> getAllTransactionGroupedWithSellerEmailOrderedwithHighProfit()
-    {
-        return  null;
+    public List<Transaction> getAllTransactionGroupedWithSellerEmailOrderedwithHighProfit() throws SQLException, ClassNotFoundException {
+        Connection connection = null;
+        connection = Config.getInstance().getConnection();
+
+        String sql = String.format("SELECT  * FROM  MARKETTRANSACTIONS WHERE  EMAIL='%s'  ORDER  by TRANSACTION_PROFIT Desc ",
+                Auth.getInstance().getCurrentUser());
+        PreparedStatement statement = null;
+        statement = connection.prepareStatement(sql);
+
+        ResultSet SET = statement.executeQuery(sql);
+        List<Transaction> transactions = new ArrayList<>(20);
+        while (SET.next()) {
+            int transactio_id = SET.getInt("Transaction_id");
+            String productName = SET.getString("T_PRODUCT_NAME");
+            int sellingPrice = SET.getInt("SELLING_PRICE");
+            int quantity = SET.getInt("QUANTITY");
+            int profit = SET.getInt("TRANSACTION_PROFIT");
+            String createdAt = SET.getString("TRANSACTION_DATE");
+
+
+            Transaction transaction = Transaction.newTransaction().id(transactio_id).
+                    productName(productName).
+                    sellingPrioce(sellingPrice).
+                    quantity(quantity).
+                    profit(profit).
+                    created_at(createdAt)
+                    .build();
+
+            transactions.add(transaction);
+        }
+        return transactions;
     }
 }
