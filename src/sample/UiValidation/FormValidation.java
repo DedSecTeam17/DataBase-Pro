9860c2da435f41fae019b3eef6f061ea78f58ff3
Sherlocked1 <40625231+Sherlocked1@users.noreply.github.com>
2018-09-10 19:34:33 +0200

    Fixed Panes sizes and positions + removed duplicates in formValidation

--- src/sample/UiValidation/FormValidation.java
+++ src/sample/UiValidation/FormValidation.java
@@ -10,64 +10,37 @@ public class FormValidation {
     }
     private FormValidation() {
     }
+
     //#region Fields Regex Checks
     public boolean checkPassword(String text) {
         String theRegex = "[A-Za-z0-9]{6,}";
-        Pattern checkRegex = Pattern.compile(theRegex);
-        Matcher match = checkRegex.matcher(text);
-        while (match.find()) {
-            if (match.group().length() != 0 && match.start() == 0)
-                return true;
-        }
-        return false;
+        return checkRegex(theRegex,text);
     }
 
     public boolean checkEmail(String text) {
         String theRegex = "[A-Za-z0-9.-_]+@[A-Za-z]+\\.[A-Za-z]{2,3}";
-        Pattern checkRegex = Pattern.compile(theRegex);
-
-        Matcher match = checkRegex.matcher(text);
-
-        while (match.find()) {
-            if (match.group().length() != 0 && match.start() == 0)
-                return true;
-        }
-
-        return false;
+        return checkRegex(theRegex,text);
     }
 
     public boolean checkuserName(String text) {
         String theRegex = "\\w{6,15}\\w";
-        Pattern checkRegex = Pattern.compile(theRegex);
-
-        Matcher match = checkRegex.matcher(text);
-
-        while (match.find()) {
-            if (match.group().length() != 0 && match.start() == 0)
-                return true;
-        }
-
-        return false;
+        return checkRegex(theRegex,text);
     }
     //checks for numbers only with no spaces
     public boolean isNumberOnly(String text) {
         String theRegex = "[0-9]+$";
-        Pattern checkRegex = Pattern.compile(theRegex);
-
-        Matcher match = checkRegex.matcher(text);
-
-        while (match.find()) {
-            if (match.group().length() != 0 && match.start() == 0)
-                return true;
-        }
-
-        return false;
+        return checkRegex(theRegex,text);
     }
     //checks for letters only with or without spaces
     public boolean isLettersOnly(String text) {
         String theRegex = "[A-Za-z\\s]+$";
-        Pattern checkRegex = Pattern.compile(theRegex);
+        return checkRegex(theRegex,text);
+    }
+    //#endregion
 
+    private boolean checkRegex(String theRegex, String text)
+    {
+        Pattern checkRegex = Pattern.compile(theRegex);
         Matcher match = checkRegex.matcher(text);
 
         while (match.find()) {
