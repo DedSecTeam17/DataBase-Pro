e3c5bdad8dc795076bf7d89e325d238c475f5ef3
Mohammed Elamin <melamin100@yahoo.com>
2018-09-13 03:57:59 -0700

    upload for fixing kharasha stuff

--- src/sample/Query/sql.sql
+++ src/sample/Query/sql.sql
@@ -1,16 +1,30 @@
-
-
+-- auto-generated definition
 create table MARKETUSER
 (
-  FNAME VARCHAR2(30) not null,
-  LNAME VARCHAR2(30),
-  PASSWORD VARCHAR2(60) default NULL,
+  FNAME      VARCHAR2(30) not null,
+  LNAME      VARCHAR2(30),
+  PASSWORD   VARCHAR2(60) default NULL,
   CREATED_AT TIMESTAMP(6),
-  EMAIL VARCHAR2(30) not null
+  EMAIL      VARCHAR2(30) not null
     primary key,
-  ROLE VARCHAR2(30) not null
+  ROLE       VARCHAR2(30) not null
 )
-/
+;
+
+
+
+create table MARKETCATEGORY
+(
+  CAT_ID   NUMBER       not null
+    primary key,
+  CAT_NAME VARCHAR2(30) not null,
+  EMAIL    VARCHAR2(30) not null
+    constraint M_CATEGORY_M_EMAIL_FK
+    references MARKETUSER
+)
+;
+
+
 
 create table MARKET_PRODUCT
 (
@@ -28,28 +42,38 @@ create table MARKET_PRODUCT
   CAT_ID             NUMBER
     constraint MPRODUCT_MCATEGORY_CAT_ID_FK
     references MARKETCATEGORY
-)/
+);
 
-create table MARKETCATEGORY
+
+
+
+
+
+
+
+
+
+
+create table MARKET_TRANSACTIONS
 (
-  CAT_ID NUMBER not null
+  TRANSACTION_ID            NUMBER not null
     primary key,
-  CAT_NAME VARCHAR2(30) not null,
-  EMAIL VARCHAR2(30) not null
-    constraint M_CATEGORY_M_EMAIL_FK
-    references MARKETUSER
+  TRANSACTION_USER_EMAIL    VARCHAR2(30)
+    constraint SYS_C006190
+    references MARKETUSER,
+  TRANSACTION_PRODUCT_NAME  VARCHAR2(30)
+    constraint SYS_C006191
+    references MARKETPRODUCT,
+  TRANSACTION_SELLING_PRICE NUMBER not null,
+  TRANSACTION_QUANTITY      NUMBER not null,
+  TRANSACTION_PROFIT        NUMBER not null,
+  TRANSACTION_DATE          TIMESTAMP(6)
 );
 
-create table MARKETTRANSACTIONS
-(
-  TRANSACTION_ID NUMBER NOT NULL PRIMARY KEY,
-  TRANSACTION_USER_EMAIL VARCHAR2(30) REFERENCES MARKETUSER,
-  TRANSACTION_PRODUCT_NAME VARCHAR2(30) REFERENCES MARKETPRODUCT,
-  TRANSACTION_SELLING_PRICE NUMBER NOT NULL ,
-  TRANSACTION_QUANTITY NUMBER NOT NULL,
-  TRANSACTION_PROFIT NUMBER NOT NULL,
-  TRANSACTION_DATE TIMESTAMP
-);
 
-/
+
+
+
+
+
 
